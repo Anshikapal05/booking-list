e09178723a7854497648a1a3d67f9ec2ec7a8397
Anshikapal05 <anshikapal546@gmail.com>
2025-05-17 11:05:47 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-  apiKey: process.env.REACT_APIKEY,
-  authDomain: process.env.REACT_AUTHDOMAIN,
-  projectId: process.env.REACT_PROJECTID,
-  storageBucket: process.env.REACT_STORBUCKET,
-  messagingSenderId: process.env.REACT_MSGSENDID,
-  appId: process.env.REACT_APPID,
-  measurementId: process.env.REACT_MEASUREID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
